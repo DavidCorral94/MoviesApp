4ff9220cdea5394c0b534e79a9c5248efc665ba9
David Corral <davidcp94@hotmail.com>
2020-07-08 19:29:26 +0200

    POST /login index.js

--- index.js
+++ index.js
@@ -37,6 +37,30 @@ app.post('/signup', passport.authenticate('signup', { session: false }), async (
     });
 });
 
+app.post('/login', async (req, res, next) => {
+    passport.authenticate('login', async (err, user, info) => {
+        try {
+            if (err) {
+                const error = new Error('An Error occurred')
+                return next(error);
+            } else if (!user) {
+                return res.json(info);
+            }
+            req.login(user, { session: false }, async (error) => {
+                if (error) return next(error);
+                //We don't want to store the sensitive information such as the
+                //user password in the token so we pick only the email and id
+                const body = { _id: user._id, email: user.email };
+                //Sign the JWT token and populate the payload with the user email and id
+                const token = jwt.sign({ user: body }, 'top_secret');
+                return res.json({ token });
+            });
+        } catch (error) {
+            return next(error);
+        }
+    })(req, res, next);
+});
+
 
 const server = http.createServer(app);
 
@@ -52,8 +76,8 @@ moviesService.connectDb((err) => {
             process.exit(1);
         }*/
 
-        server.listen(PORT, () => {
-            console.log('Server up and running on localhost:' + PORT);
-        });
+    server.listen(PORT, () => {
+        console.log('Server up and running on localhost:' + PORT);
+    });
     //});
 });
