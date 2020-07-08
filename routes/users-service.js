bacbc3e0951feb5d56e07b4f23fb3d8e1460f28a
David Corral <davidcp94@hotmail.com>
2020-07-08 18:46:57 +0200

    Connection to db in users-service

--- routes/users-service.js
+++ routes/users-service.js
@@ -4,3 +4,16 @@ const MongoClient = require('mongodb').MongoClient;
 let db;
 let ObjectId = require('mongodb').ObjectID;
 const Users = function() {};
+
+Movies.prototype.connectDb = function(callback) {
+    MongoClient.connect("mongodb+srv://test:test123@ief-2020-dcp.58msp.mongodb.net/MoviesApp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true },
+        function(err, database) {
+            if (err) {
+                callback(err);
+            }
+
+            db = database.db('MoviesApp').collection('users');
+
+            callback(err, database);
+        });
+};
