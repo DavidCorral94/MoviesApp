0cada8e7029a7e90242831e544a9a4528390ca03
David Corral <davidcp94@hotmail.com>
2020-07-08 19:06:14 +0200

    middleware signup

--- auth.js
+++ auth.js
@@ -4,3 +4,17 @@ const localStrategy = require('passport-local').Strategy;
 const usersService = require('./routes/users-service');
 const JWTstrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+//Create a passport middleware to handle user registration
+passport.use('signup', new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done) => {
+    try {
+        let user = { email: email, password: password };
+        await usersService.add(user);
+        return done(null, user);
+    } catch (error) {
+        done(error);
+    }
+}));
