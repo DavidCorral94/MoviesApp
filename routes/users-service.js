50c4ec713a2cc4da09c15a13f48976199e6d9394
David Corral <davidcp94@hotmail.com>
2020-07-08 18:57:20 +0200

    users-service.js finished

--- routes/users-service.js
+++ routes/users-service.js
@@ -5,7 +5,7 @@ let db;
 let ObjectId = require('mongodb').ObjectID;
 const Users = function() {};
 
-Movies.prototype.connectDb = function(callback) {
+Users.prototype.connectDb = function(callback) {
     MongoClient.connect("mongodb+srv://test:test123@ief-2020-dcp.58msp.mongodb.net/MoviesApp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true },
         function(err, database) {
             if (err) {
@@ -17,3 +17,28 @@ Movies.prototype.connectDb = function(callback) {
             callback(err, database);
         });
 };
+
+Users.prototype.add = (user) => {
+    return add(user);
+};
+
+async function add(user) {
+    return db.insertOne(user);
+}
+
+Users.prototype.find = (user) => {
+    return find(user);
+};
+
+async function find(user) {
+    return new Promise((resolve, reject) => {
+        db.find(user).toArray((err, data) => {
+            err
+                ?
+                reject(err) :
+                resolve(data);
+        });
+    });
+}
+
+module.exports = new Users();
