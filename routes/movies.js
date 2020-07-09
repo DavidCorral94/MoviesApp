34540efc06f4f069fe6cbe4343fe1f9437da6d3c
David Corral <davidcp94@hotmail.com>
2020-07-09 18:39:40 +0200

    add encrypt.js require in movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 const moviesService = require('./movies-service');
+const encryption = require('./encrypt.js');
+
 
 /*********** /movies ***********/
 
