74cfff63c45b501ba50cf3df079e0be813b0e980
David Corral <davidcp94@hotmail.com>
2020-07-09 18:31:18 +0200

    encrypt.js

---
+++ routes/encrypt.js
@@ -0,0 +1,5 @@
+const Crypto = require("crypto");
+const ENCRYPTION_KEY = "CLAVE DE CIFRADO DE 32 CARACTERES/256 BITS"
+const IV = Buffer.alloc(16, 0);
+
+// https://www.allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx
